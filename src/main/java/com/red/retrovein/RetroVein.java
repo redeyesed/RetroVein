326fc62bdd4c5d7e31fb7ce772f647a04568da83
theRedeyesed <redeyesed@mail.ru>
2026-08-29 20:43:57 +0300

    Adding everything to Git.

---
+++ src/main/java/com/red/retrovein/RetroVein.java
@@ -0,0 +1,10 @@
+package com.red.retrovein;
+
+public final class RetroVein {
+	private RetroVein() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("RetroVein 0.1.0");
+	}
+}
