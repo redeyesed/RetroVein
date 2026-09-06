48a123255cebff62af6215ad9bd317f17a6aaa7b
theRedeyesed <redeyesed@mail.ru>
2026-09-06 20:19:55 +0300

    Перенос классов в корневой пакет, по умолчанию.

--- src/main/java/com/red/retrovein/mapping/ClassMapper.java
+++ src/main/java/com/red/retrovein/mapping/ClassMapper.java
@@ -17,19 +17,94 @@ public final class ClassMapper {
 	public Map<String, String> build(List<ClassInfo> classInfos) {
 		Map<String, String> mappings = new HashMap<String, String>();
 
+		String rootPackage = findRootPackage(classInfos);
+
 		for (ClassInfo classInfo : classInfos) {
 
 			String originalName = classInfo.getName();
 
-			String mappedName = nameGenerator.next();
+			String mappedName = rootPackage + "/" + nameGenerator.next();
 
 			mappings.put(originalName, mappedName);
 
 			RetroLogger.debug("Class mapping: {} -> {}", originalName, mappedName);
 		}
 
-		RetroLogger.debug("Generated {} class mappings", mappings.size());
+		RetroLogger.debug("Generated {} class mappings in package {}", mappings.size(), rootPackage);
 
 		return mappings;
+	}
+
+	/*
+	 * Определяет общий корневой пакет всех классов.
+	 */
+	private String findRootPackage(List<ClassInfo> classInfos) {
+		String rootPackage = null;
+
+		for (ClassInfo classInfo : classInfos) {
+
+			String className = classInfo.getName();
+
+			int lastSlash = className.lastIndexOf('/');
+
+			if (lastSlash <= 0) {
+				continue;
+			}
+
+			String packageName = className.substring(0, lastSlash);
+
+			if (rootPackage == null) {
+
+				rootPackage = packageName;
+
+				continue;
+			}
+
+			rootPackage = findCommonPackage(rootPackage, packageName);
+
+			if (rootPackage.isEmpty()) {
+				break;
+			}
+		}
+
+		return rootPackage == null ? "" : rootPackage;
+	}
+
+	/*
+	 * Находит общий пакет двух указанных пакетов.
+	 */
+	private String findCommonPackage(String first, String second) {
+		String[] firstParts = first.split("/");
+		String[] secondParts = second.split("/");
+
+		int length = Math.min(firstParts.length, secondParts.length);
+
+		int commonLength = 0;
+
+		for (int i = 0; i < length; i++) {
+
+			if (!firstParts[i].equals(secondParts[i])) {
+				break;
+			}
+
+			commonLength++;
+		}
+
+		if (commonLength == 0) {
+			return "";
+		}
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < commonLength; i++) {
+
+			if (i > 0) {
+				result.append('/');
+			}
+
+			result.append(firstParts[i]);
+		}
+
+		return result.toString();
 	}
 }
