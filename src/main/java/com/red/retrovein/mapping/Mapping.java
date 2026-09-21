8fca9069b26711035f86bdc3ba2f03856b2eeb14
theRedeyesed <redeyesed@mail.ru>
2026-09-21 11:33:49 +0300

    Рефакторинг: Структура MappingBuilder / Mapping.

--- src/main/java/com/red/retrovein/mapping/Mapping.java
+++ src/main/java/com/red/retrovein/mapping/Mapping.java
@@ -1,75 +1,74 @@
 package com.red.retrovein.mapping;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class Mapping {
 	private final Map<String, String> classes;
-	private final Map<String, String> methods;
 	private final Map<String, String> fields;
+	private final Map<String, String> methods;
 	private final Map<String, String> localVariables;
 	private final Map<String, String> enums;
 
-	public Mapping(Map<String, String> classes, Map<String, String> methods, Map<String, String> fields,
+	/*
+	 * Инициализирует все типы и оборачивает их в неизменяемые Map, исключая
+	 * возможность модификации состояния извне.
+	 */
+	public Mapping(Map<String, String> classes, Map<String, String> fields, Map<String, String> methods,
 			Map<String, String> localVariables, Map<String, String> enums) {
-		this.classes = Collections.unmodifiableMap(classes);
-		this.methods = Collections.unmodifiableMap(methods);
-		this.fields = Collections.unmodifiableMap(fields);
-		this.localVariables = Collections.unmodifiableMap(localVariables);
-		this.enums = Collections.unmodifiableMap(enums);
+		this.classes = Collections.unmodifiableMap(new HashMap<String, String>(classes));
+		this.fields = Collections.unmodifiableMap(new HashMap<String, String>(fields));
+		this.methods = Collections.unmodifiableMap(new HashMap<String, String>(methods));
+		this.localVariables = Collections.unmodifiableMap(new HashMap<String, String>(localVariables));
+		this.enums = Collections.unmodifiableMap(new HashMap<String, String>(enums));
 	}
 
 	public String getClassName(String originalName) {
-		String mappedName = classes.get(originalName);
-
+		String mappedName = this.classes.get(originalName);
 		return mappedName != null ? mappedName : originalName;
-	}
-
-	public String getMethodName(String owner, String name, String descriptor) {
-		String key = owner + "." + name + descriptor;
-		String mappedName = methods.get(key);
-
-		return mappedName != null ? mappedName : name;
 	}
 
 	public String getFieldName(String owner, String name, String descriptor) {
 		String key = owner + "." + name + ":" + descriptor;
-		String mappedName = fields.get(key);
+		String mappedName = this.fields.get(key);
+		return mappedName != null ? mappedName : name;
+	}
 
+	public String getMethodName(String owner, String name, String descriptor) {
+		String key = owner + "." + name + descriptor;
+		String mappedName = this.methods.get(key);
 		return mappedName != null ? mappedName : name;
 	}
 
 	public String getLocalVariableName(String owner, String methodName, String methodDescriptor, int index) {
 		String key = owner + "." + methodName + methodDescriptor + "#" + index;
-		String mappedName = localVariables.get(key);
-
-		return mappedName != null ? mappedName : null;
+		return this.localVariables.get(key);
 	}
 
 	public String getEnumName(String owner, String name, String descriptor) {
 		String key = EnumMapper.createKey(owner, name, descriptor);
-		String mappedName = enums.get(key);
-
+		String mappedName = this.enums.get(key);
 		return mappedName != null ? mappedName : name;
 	}
 
 	public Map<String, String> getClasses() {
-		return classes;
-	}
-
-	public Map<String, String> getMethods() {
-		return methods;
+		return this.classes;
 	}
 
 	public Map<String, String> getFields() {
-		return fields;
+		return this.fields;
+	}
+
+	public Map<String, String> getMethods() {
+		return this.methods;
 	}
 
 	public Map<String, String> getLocalVariables() {
-		return localVariables;
+		return this.localVariables;
 	}
 
 	public Map<String, String> getEnums() {
-		return enums;
+		return this.enums;
 	}
 }
--- src/main/java/com/red/retrovein/mapping/MappingBuilder.java
+++ src/main/java/com/red/retrovein/mapping/MappingBuilder.java
@@ -1,9 +1,5 @@
 package com.red.retrovein.mapping;
 
-import com.red.retrovein.io.ClassInfo;
-import com.red.retrovein.logging.LogCategory;
-import com.red.retrovein.logging.RetroLogger;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,6 +8,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import com.red.retrovein.io.ClassInfo;
+import com.red.retrovein.logging.LogCategory;
+import com.red.retrovein.logging.RetroLogger;
 
 public final class MappingBuilder {
 	private final ClassMapper classMapper;
@@ -28,151 +28,151 @@ public final class MappingBuilder {
 		this.enumMapper = new EnumMapper();
 	}
 
+	/*
+	 * Строит полный mapping для переданного набора классов. Сначала
+	 * подготавливаются классы и metadata, после чего создаются все остальные.
+	 */
 	public Mapping build(List<ClassInfo> classInfos) {
 		RetroLogger.debug(LogCategory.Mapping, "Preparing {} classes for mapping", classInfos.size());
-		List<ClassInfo> sortedClasses = sortClasses(classInfos);
-		RetroLogger.debug(LogCategory.Mapping, "Classes sorted alphabetically");
-		/*
-		 * Classes.
-		 */
-		RetroLogger.debug(LogCategory.Mapping, "Generating class mappings");
-		Map<String, String> classes = classMapper.build(sortedClasses);
-		RetroLogger.debug(LogCategory.Mapping, "Generated {} class mappings", classes.size());
+		List<ClassInfo> sortedClasses = this.sortClasses(classInfos);
+		Map<String, String> classes = this.buildClassMappings(sortedClasses);
+		Map<String, ClassMetadata> metadata = this.buildMetadata(sortedClasses);
+		Map<String, String> fields = this.buildFieldMappings(sortedClasses);
+		Map<String, String> methods = this.buildMethodMappings(sortedClasses, metadata);
+		Map<String, String> localVariables = this.buildLocalVariableMappings(sortedClasses);
+		Map<String, String> enums = this.buildEnumMappings(sortedClasses);
 
-		/*
-		 * Metadata.
-		 */
-		RetroLogger.debug(LogCategory.Mapping, "Generating class metadata");
-		Map<String, ClassMetadata> metadata = methodMapper.buildMetadata(sortedClasses);
-		RetroLogger.debug(LogCategory.Mapping, "Generated metadata for {} classes", metadata.size());
-
-		/*
-		 * Fields.
-		 */
-		RetroLogger.debug(LogCategory.Mapping, "Generating field mappings");
-		Map<String, String> fields = fieldMapper.build(sortedClasses);
-		RetroLogger.debug(LogCategory.Mapping, "Generated {} field mappings", fields.size());
-
-		/*
-		 * Methods.
-		 */
-		RetroLogger.debug(LogCategory.Mapping, "Generating method mappings");
-		List<ClassInfo> methodClasses = sortByInheritance(sortedClasses, metadata);
-		Map<String, String> methods = methodMapper.build(methodClasses, metadata);
-		RetroLogger.debug(LogCategory.Mapping, "Generated {} method mappings", methods.size());
-		/*
-		 * Local variables.
-		 */
-		RetroLogger.debug(LogCategory.Mapping, "Generating local variable mappings");
-		Map<String, String> localVariables = localVariableMapper.build(sortedClasses);
-		RetroLogger.debug(LogCategory.Mapping, "Generated {} local variable mappings", localVariables.size());
-
-		/*
-		 * Enum constants.
-		 */
-		RetroLogger.debug(LogCategory.Mapping, "Generating enum mappings");
-		Map<String, String> enums = enumMapper.build(sortedClasses);
-		RetroLogger.debug(LogCategory.Mapping, "Generated {} enum mappings", enums.size());
-
-		RetroLogger.info(LogCategory.Mapping,
-				"Generated mappings: {} classes, {} fields, {} methods, {} variables, {} enum constants",
-				classes.size(), fields.size(), methods.size(), localVariables.size(), enums.size());
-
+		this.logMappingSummary(classes, fields, methods, localVariables, enums);
 		return new Mapping(classes, methods, fields, localVariables, enums);
 	}
 
+	private Map<String, String> buildClassMappings(List<ClassInfo> classes) {
+		RetroLogger.debug(LogCategory.Mapping, "Generating class mappings");
+		Map<String, String> mappings = classMapper.build(classes);
+		RetroLogger.debug(LogCategory.Mapping, "Generated {} class mappings", mappings.size());
+		return mappings;
+	}
+
+	private Map<String, ClassMetadata> buildMetadata(List<ClassInfo> classes) {
+		RetroLogger.debug(LogCategory.Mapping, "Generating class metadata");
+		Map<String, ClassMetadata> metadata = methodMapper.buildMetadata(classes);
+		RetroLogger.debug(LogCategory.Mapping, "Generated metadata for {} classes", metadata.size());
+		return metadata;
+	}
+
+	private Map<String, String> buildFieldMappings(List<ClassInfo> classes) {
+		RetroLogger.debug(LogCategory.Mapping, "Generating field mappings");
+		Map<String, String> mappings = fieldMapper.build(classes);
+		RetroLogger.debug(LogCategory.Mapping, "Generated {} field mappings", mappings.size());
+		return mappings;
+	}
+
+	private Map<String, String> buildMethodMappings(List<ClassInfo> sortedClasses,
+			Map<String, ClassMetadata> metadata) {
+		RetroLogger.debug(LogCategory.Mapping, "Generating method mappings");
+		List<ClassInfo> inheritanceOrder = sortByInheritance(sortedClasses, metadata);
+		Map<String, String> mappings = methodMapper.build(inheritanceOrder, metadata);
+		RetroLogger.debug(LogCategory.Mapping, "Generated {} method mappings", mappings.size());
+		return mappings;
+	}
+
+	private Map<String, String> buildLocalVariableMappings(List<ClassInfo> classes) {
+		RetroLogger.debug(LogCategory.Mapping, "Generating local variable mappings");
+		Map<String, String> mappings = localVariableMapper.build(classes);
+		RetroLogger.debug(LogCategory.Mapping, "Generated {} local variable mappings", mappings.size());
+		return mappings;
+	}
+
+	private Map<String, String> buildEnumMappings(List<ClassInfo> classes) {
+		RetroLogger.debug(LogCategory.Mapping, "Generating enum mappings");
+		Map<String, String> mappings = enumMapper.build(classes);
+		RetroLogger.debug(LogCategory.Mapping, "Generated {} enum mappings", mappings.size());
+		return mappings;
+	}
+
+	/*
+	 * Выводит сводную статистику по количеству mappings, созданных каждым
+	 * компонентом.
+	 */
+	private void logMappingSummary(Map<String, String> classes, Map<String, String> fields, Map<String, String> methods,
+			Map<String, String> localVariables, Map<String, String> enums) {
+		RetroLogger.info(LogCategory.Mapping,
+				"Generated mappings: {} classes, {} fields, {} methods, {} variables, {} enum constants",
+				classes.size(), fields.size(), methods.size(), localVariables.size(), enums.size());
+	}
+
+	/*
+	 * Создаёт копию входного списка и сортирует классы по имени, обеспечивая
+	 * детерминированный порядок их обработки.
+	 */
 	private List<ClassInfo> sortClasses(List<ClassInfo> classInfos) {
-
 		List<ClassInfo> sorted = new ArrayList<ClassInfo>(classInfos);
-
 		Collections.sort(sorted, new Comparator<ClassInfo>() {
 			@Override
 			public int compare(ClassInfo first, ClassInfo second) {
-
 				return first.getName().compareTo(second.getName());
 			}
 		});
-
+		RetroLogger.debug(LogCategory.Mapping, "Classes sorted alphabetically");
 		return sorted;
 	}
 
 	/**
 	 * Сортирует классы так, чтобы родительские классы и интерфейсы обрабатывались
-	 * раньше классов, которые от них наследуются.
-	 *
-	 * Это необходимо для корректного переиспользования имён переопределённых
-	 * методов.
+	 * раньше классов, которые от них наследуются. Это необходимо для корректного
+	 * переиспользования имён переопределённых методов.
 	 */
 	private List<ClassInfo> sortByInheritance(List<ClassInfo> sortedClasses, Map<String, ClassMetadata> metadata) {
 		Map<String, ClassInfo> classesByName = new HashMap<String, ClassInfo>();
-
 		for (ClassInfo classInfo : sortedClasses) {
 			classesByName.put(classInfo.getName(), classInfo);
 		}
-
-		List<ClassInfo> result = new ArrayList<ClassInfo>();
+		List<ClassInfo> orderedClasses = new ArrayList<ClassInfo>();
 		Set<String> visited = new HashSet<String>();
 		Set<String> visiting = new HashSet<String>();
-
 		for (ClassInfo classInfo : sortedClasses) {
-			visitClass(classInfo.getName(), classesByName, metadata, visited, visiting, result);
+			this.visitClass(classInfo.getName(), classesByName, metadata, visited, visiting, orderedClasses);
 		}
-
-		return result;
+		return orderedClasses;
 	}
 
-	/**
-	 * Рекурсивно добавляет родительские классы и интерфейсы перед текущим классом.
+	/*
+	 * Рекурсивно обходит иерархию класса и добавляет его после всех родительских
+	 * классов и интерфейсов.
 	 */
 	private void visitClass(String className, Map<String, ClassInfo> classesByName, Map<String, ClassMetadata> metadata,
-			Set<String> visited, Set<String> visiting, List<ClassInfo> result) {
+			Set<String> visited, Set<String> visiting, List<ClassInfo> orderedClasses) {
 		if (visited.contains(className)) {
 			return;
 		}
 
 		/*
-		 * Корректная Java-иерархия не должна содержать циклов. Проверка защищает от
-		 * бесконечной рекурсии на повреждённом bytecode.
+		 * Защищает рекурсивный обход от циклов в графе наследования.
 		 */
 		if (!visiting.add(className)) {
 			RetroLogger.warn(LogCategory.Mapping, "Inheritance cycle detected involving {}", className);
 			return;
 		}
-
 		ClassMetadata classMetadata = metadata.get(className);
 
 		if (classMetadata != null) {
 			String superName = classMetadata.getSuperName();
-
-			/*
-			 * Сначала обрабатываем родительский класс.
-			 */
 			if (superName != null && classesByName.containsKey(superName)) {
-				visitClass(superName, classesByName, metadata, visited, visiting, result);
+				this.visitClass(superName, classesByName, metadata, visited, visiting, orderedClasses);
 			}
-
-			/*
-			 * Интерфейсы обрабатываем в алфавитном порядке, чтобы результат не зависел от
-			 * порядка их объявления.
-			 */
 			List<String> interfaces = new ArrayList<String>(classMetadata.getInterfaces());
-
-			Collections.sort(interfaces);
-
+			interfaces.sort(String::compareTo);
 			for (String interfaceName : interfaces) {
 				if (classesByName.containsKey(interfaceName)) {
-					visitClass(interfaceName, classesByName, metadata, visited, visiting, result);
+					this.visitClass(interfaceName, classesByName, metadata, visited, visiting, orderedClasses);
 				}
 			}
 		}
-
 		visiting.remove(className);
 		visited.add(className);
-
 		ClassInfo classInfo = classesByName.get(className);
-
 		if (classInfo != null) {
-			result.add(classInfo);
+			orderedClasses.add(classInfo);
 		}
 	}
 }
